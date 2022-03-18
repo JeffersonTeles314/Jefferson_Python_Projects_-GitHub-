aaf2da5fd26c6b7d98e55a782c52ea9a6ca0bdcd
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2022-03-18 11:52:27 -0500

    Create pg_003.py

---
+++ pg_003.py
@@ -0,0 +1,9 @@
+from random import randint
+var1=randint(1,6)
+print('---{}---'.format(var1))
+if var1==6 or var1==5:
+   print('very good')
+elif var1==4 or var1==3:
+   print('good')
+else :
+   print('too bad')
